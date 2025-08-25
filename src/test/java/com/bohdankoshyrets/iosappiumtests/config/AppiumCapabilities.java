63ea1e360636da66cb7c940e26b670dc7663701d
Bohdan Koshyrets <36152000+bohdankoshyrets@users.noreply.github.com>
2025-08-25 17:18:08 +0300

    move caps arguments externally (#4)

--- src/test/java/com/bohdankoshyrets/iosappiumtests/config/AppiumCapabilities.java
+++ src/test/java/com/bohdankoshyrets/iosappiumtests/config/AppiumCapabilities.java
@@ -1,36 +1,19 @@
 package com.bohdankoshyrets.iosappiumtests.config;
 
 import io.appium.java_client.ios.options.XCUITestOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
-
 import java.time.Duration;
 
 public class AppiumCapabilities {
-    private static final String PLATFORM_NAME = "iOS";
-    private static final String PLATFORM_VERSION = "18.6";
-    private static final String DEVICE_NAME = "iPhone 16";
-    private static final String AUTOMATION_NAME = "XCUITest";
-
-    public static DesiredCapabilities getCapabilities() {
-        DesiredCapabilities caps = new DesiredCapabilities();
-        caps.setCapability("appium:platformName", PLATFORM_NAME);
-        caps.setCapability("appium:platformVersion", PLATFORM_VERSION);
-        caps.setCapability("appium:deviceName", DEVICE_NAME);
-        caps.setCapability("appium:automationName", AUTOMATION_NAME);
-        caps.setCapability("appium:bundleId", BundleID.preferences);
-        return caps;
-    }
-
     public static XCUITestOptions getOptions() {
         String udid = System.getProperty("appium.device.udid");
         XCUITestOptions opts = new XCUITestOptions();
 
-        opts.setPlatformName(PLATFORM_NAME);
-        opts.setWdaLaunchTimeout(Duration.ofSeconds(120));
-        opts.setShowXcodeLog(true);
+        opts.setCapability("appium:platformVersion", TestConfig.get("appium.platformVersion"));
+        opts.setCapability("appium:deviceName", TestConfig.get("appium.deviceName"));
+        opts.setPlatformName(TestConfig.get("appium.platformName"));
+        opts.setWdaLaunchTimeout(Duration.ofSeconds(180));
         opts.setBundleId(BundleID.preferences);
-        opts.setCapability("appium:platformVersion", PLATFORM_VERSION);
-        opts.setCapability("appium:deviceName", DEVICE_NAME);
+        opts.setShowXcodeLog(false);
 
         if (udid != null) {
             opts.setUdid(udid);
