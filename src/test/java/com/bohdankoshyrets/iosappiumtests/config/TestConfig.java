17fe1b3188f05a773807a840b3cf3affc8a76b1f
Bohdan Boshyrets <bohdankoshyrets@icloud.com>
2025-08-25 16:02:23 +0300

    move caps arguments externally

---
+++ src/test/java/com/bohdankoshyrets/iosappiumtests/config/TestConfig.java
@@ -0,0 +1,24 @@
+package com.bohdankoshyrets.iosappiumtests.config;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class TestConfig {
+    final private static Properties PROPS = new Properties();
+
+    static {
+        try {
+            InputStream input = TestConfig.class.getClassLoader().getResourceAsStream("appium.properties");
+            if (input != null) {
+                PROPS.load(input);
+            }
+        } catch(IOException e) {
+            throw new RuntimeException("cannot load appium.properties", e);
+        }
+    }
+
+    public static String get(String key) {
+        return System.getProperty(key, PROPS.getProperty(key));
+    }
+}
